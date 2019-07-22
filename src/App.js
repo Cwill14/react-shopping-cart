049961e3d00df5548cd0c90cb799917066127b37
Clark Williams <theclarkwilliams@gmail.com>
2019-07-22 14:28:01 -0600

    removeItem stretch goal complete

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 import { ProductContext } from './contexts/ProductContext';
 import { CartContext } from './contexts/CartContext';
+import { ItemContext } from './contexts/ItemContext';
 
 function App() {
 	const [products] = useState(data);
@@ -18,13 +19,19 @@ function App() {
 		setCart([...cart, item]);
 	};
 
+	const removeItem = id => {
+		setCart(cart.filter(item => item.id !== id))
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }} >
 				<CartContext.Provider value={{ cart }} >
-					<Navigation />
-					<Route exact path="/" component={Products} />
-					<Route path="/cart" component={ShoppingCart} />
+					<ItemContext.Provider value={{ removeItem }} >
+						<Navigation />
+						<Route exact path="/" component={Products} />
+						<Route path="/cart" component={ShoppingCart} />
+					</ItemContext.Provider>
 				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
